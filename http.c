dfafb0f2fe6e3ffc552384f4903bc7fe2e9c971d
sebatt90 <me@example.com>
2024-04-23 17:38:05 +0200

    Server partially re-written

--- http.c
+++ http.c
@@ -1,4 +1,3 @@
-#include "sys/types.h"
 #include "sys/socket.h"
 #include <netinet/in.h>
 #include <string.h>
@@ -9,99 +8,74 @@
 
 #include "http.h"
 
+extern inline void catch_error(int condition, char* msg){
+    if(condition == -1){
+        fprintf(stderr, "(ERROR) %s\n", msg);
+        exit(-1);
+    }
+}
+
+char* req_error(){
+    fprintf(stderr, "(ERROR) HTTP Request Error");
+    return "HTTP/1.1 200 OK\nServer:httpc\n\n<h1>HTTP Request Error</h1>";
+}
+
 int sfd;
 
 char* folder;
 
-void http_head(const char* str, char** method, char** location){
-    int i = 0;
-    int sstr = 0;
-    
-    char* buf = (char*) calloc(sizeof(str),sizeof(char));
-
-    char** point[3] = {method, location};
-
-    while (*(str+i) != '\n') {
-        //printf("%c\n", str[i]);
-        char ch = *(str+i);
-        i++;
-        if(ch == ' '){
-            char** d = point[sstr];
-            *(d) = (char*) malloc(strlen(buf)+1);
-            strncpy(*d, buf, strlen(buf)+1);
-
-            // clear
-            memset(buf, 0, sizeof(str));
-            sstr++;
-            continue;
-        }
-
-        strncat(buf, &ch, sizeof(char));
-        
-    }
-
-    free(buf);
-}
-
-char* get_src(const char* filepath){
-    char* res_body;
+char* openfile(const char* filepath){
     char* fpath = (char*) malloc(strlen(filepath)+strlen(folder)+1);
 
     strncpy(fpath, folder, strlen(folder));
     strncat(fpath, filepath, strlen(filepath)+1);
 
-    printf("path: %s\n",fpath);
-    
-    FILE* fp;
-    if(!(fp = fopen(fpath, "r"))){
-        // handle (return 404)
-        char r[] = "HTTP/1.1 404 Not Found\nServer: httpc\nContent-Type: text/html\n\n\
-        <h1><i>httpc</i></h1>\
-        <h2>404 Not Found</h2>";
-        res_body = (char*) malloc(sizeof(r));
-        strncpy(res_body, r, sizeof(r));
-        return res_body;
-        
-    }
+    FILE* fd;
+    if(!(fd=fopen(fpath, "r"))) return 0;
 
-    char* r = "HTTP/1.1 200 OK\nServer: httpc\nContent-Type: text/html\n\n";
-    fseek(fp, 0, SEEK_END);
-    long fsize = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
+    fseek(fd, 0, SEEK_END);
+    long fsize = ftell(fd);
+    fseek(fd, 0, SEEK_SET);
 
     char* str = (char*) malloc(fsize+1);
-    fread(str, fsize, 1, fp);
-
-    fclose(fp);
+    fread(str, fsize, 1, fd);
+    fclose(fd);
     str[fsize] = '\0';
 
-    res_body = (char*) malloc(strlen(r)+strlen(str)+2);
-    strcpy(res_body, r);
-    strcat(res_body, str);
-
-    free(str);
-    free(fpath);
-
-    return res_body;
+    return str;
 }
 
+
 char* process_req(char* req){
-    char* method;
-    char* location;
+    // Example: GET / HTTP/1.1
+    char* method = strtok(req, " ");
+    if(method == NULL) return req_error();
+    char* route = strtok(NULL, " ");
+    if(route == NULL) return req_error();
+    char* http_version = strtok(NULL, " ");
+    if(http_version == NULL) return req_error();
 
-    http_head(req, &method, &location);
+    char* fpath = (strcmp(route, "/")==0) ? "/index.html" : route;
+    
 
-    printf("Method: %s\nLocation: %s\n", method, location);
+    char* file = openfile(fpath);
 
-    // create a response
-    char* loc = location;
-    if(strcmp(location, "/") == 0){
-        loc = "/index.html";
+    if(file == NULL){
+        char* head = "HTTP/1.1 400 OK\nServer: httpc\nContent-Type: text/html\n\n";
+        char* res = (char*)malloc((sizeof(char)*sizeof(head))+(sizeof(char)*sizeof("404 Content Not Found")));
+        strcpy(res, head);
+        strcat(res, "404 Content Not Found\0");
+        return res;
     }
-    char* res = get_src(loc);
 
-    free(method);
-    free(location);
+    char head[] = "HTTP/1.1 200 OK\nServer: httpc\nContent-Type: text/html\n\n";
+
+    // TODO: handle multiple file types other than html...
+
+    char* res = (char*)malloc((sizeof(char)*sizeof(head))+(sizeof(char)*strlen(file))+2);
+    strncpy(res, head, sizeof(head));
+    strncat(res, file, strlen(file));
+
     return res;
 }
 
@@ -114,28 +88,19 @@ void http_init(unsigned short port, const char* content){
     sfd = socket(AF_INET, SOCK_STREAM, 0);
 
     // error while opening socket
-    if(sfd == -1){
-        fprintf(stderr, "(ERROR) An error occured while creating the HTTP socket!\n");
-        exit(-1);
-    }
+    catch_error(sfd, "An error occured while creating the TCP socket!");
 
     struct sockaddr_in addr;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
 
-    if (bind(sfd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
-        fprintf(stderr, "(ERROR) Error while binding HTTP socket!\n");
-        exit(-1);
-    }
+    catch_error(bind(sfd, (struct sockaddr *) &addr, sizeof(addr)), "Error while binding the TCP socket!");
 
     int backlog = 10;
 
     // error while listening
-    if (listen(sfd, backlog) == -1) {
-        fprintf(stderr, "(ERROR) An error has occured while trying to listen for incoming connections\n");
-        exit(-1);
-    }
+    catch_error(listen(sfd, backlog), "An error has occured while trying to listen for incoming connections!");
 
     printf("(OK) HTTP Server is listening on port %d\n", port);
 }
@@ -147,7 +112,7 @@ void http_accept() {
 
     connfd = accept(sfd, (struct sockaddr *) &cliaddr, &cliaddr_len);
     if (connfd == -1) {
-        fprintf(stderr, "(ERROR) A connection error has occured");
+        printf("A connection error has occured!\n");
         return;
     }
 
@@ -159,23 +124,18 @@ void http_accept() {
     if(blen == -1)
     {
         // handle
-        fprintf(stderr, "(ERROR) Invalid req");
-        exit(-1);
-    }
-
-    printf("Request: \n%s\n",req);
-
-    char* res = process_req(req);
-
-    // Debug response by print
-    printf("Response: \n%s\n",res);
-
-    if(write(connfd, res, strlen(res)+1) == -1){
-        // error
-        fprintf(stderr, "(ERROR) An error has occured while writing to HTTP Client\n");
+        printf("Invalid Request!\n");
         return;
     }
 
+
+    char* res = process_req(req);
+
+    if(write(connfd, res, strlen(res)+1) == -1){
+        // error
+        printf( "An error has occured while writing to HTTP Client\n");
+        return;
+    }
     free(res);
     close(connfd);
 }
